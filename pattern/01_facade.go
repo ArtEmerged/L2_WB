2e09286b6cb67d9f494478e09099593c9a03f5a9
Artyom_PC <lilapank@yandex.ru>
2024-02-14 14:40:14 +0300

    fix: golint

--- pattern/01_facade.go
+++ pattern/01_facade.go
@@ -4,7 +4,7 @@ import "fmt"
 
 /*
 	Реализовать паттерн «фасад».
-Объяснить применимость паттерна, его плюсы и минусы,а также реальные примеры использования данного примера на практике.
+	Объяснить применимость паттерна, его плюсы и минусы, а также реальные примеры использования данного примера на практике.
 	https://en.wikipedia.org/wiki/Facade_pattern
 */
 /*
@@ -16,58 +16,61 @@ import "fmt"
 	- Фасад рискует стать "супер-объектом", привязанным ко всем классам программы.
 */
 
-// Фасад для уведомлений
+// NotificationFacade фасад для уведомлений
 type NotificationFacade struct {
 	email *Email
 	app   *App
-	sms   *Sms
+	sms   *SMS
 	user  *User
 }
 
-/*
-Отправка уведомлений через все каналы связи.
-Клиенту не нужно знать всю бизнес-логику, куда и как будут отправляться уведомления.
-Для него все просто и понятно: вызывая этот метод, он отправит уведомление покупателю о готовности заказа.
-*/
-func (n *NotificationFacade) Send(userId int, orderId string) {
-	user := n.user.GetUserById(userId)
-	n.email.SendsNotice(user, orderId)
-	n.app.SendsNotice(user, orderId)
-	n.sms.SendsNotice(user, orderId)
+// Send отправяет уведомления через все каналы связи.
+// Клиенту не нужно знать всю бизнес-логику, куда и как будут отправляться уведомления.
+// Для него все просто и понятно: вызывая этот метод, он отправит уведомление покупателю о готовности заказа.
+func (n *NotificationFacade) Send(userID int, orderID string) {
+	user := n.user.GetUserByID(userID)
+	n.email.SendsNotice(user, orderID)
+	n.app.SendsNotice(user, orderID)
+	n.sms.SendsNotice(user, orderID)
 }
 
+// User обеспечивает взамодействие c User
 type User struct {
-	Id   int
+	ID   int
 	Name string
 }
 
-func (u *User) GetUserById(id int) *User {
-	return &User{id, "Петя"}
+// GetUserByID находит ures по userID
+func (u *User) GetUserByID(ID int) *User {
+	return &User{ID, "Петя"}
 }
 
+// Email обеспечивает взамодействие с email сервисом
 type Email struct{}
 
-// Отправляет уведомление пользователю по email
-func (e *Email) SendsNotice(user *User, orderId string) {
+// SendsNotice отправляет уведомление пользователю по email
+func (e *Email) SendsNotice(user *User, orderID string) {
 	// Здесь может содержаться вся бизнес-логика для отправки уведомления по email,
-	// у структуры Email могут присутствовать подобные методы (GetEmailByUserId, EmailServiceConnection)
-	fmt.Printf("[email] %s, ваш заказ:%s готов к выдаче\n", user.Name, orderId)
+	// у структуры Email могут присутствовать подобные методы (GetEmailByUserID, EmailServiceConnection)
+	fmt.Printf("[email] %s, ваш заказ:%s готов к выдаче\n", user.Name, orderID)
 }
 
+// App обеспечивает взамодействие c приложением
 type App struct{}
 
-// Отправляет уведомление пользователю в приложение
-func (a *App) SendsNotice(user *User, orderId string) {
+// SendsNotice отправляет уведомление пользователю в приложение
+func (a *App) SendsNotice(user *User, orderID string) {
 	// Здесь может содержаться вся бизнес-логика для отправки уведомления в приложение,
 	// у структуры App могут присутствовать подобные методы (AppServiceConnection)
-	fmt.Printf("[app] %s, ваш заказ:%s готов к выдаче\n", user.Name, orderId)
+	fmt.Printf("[app] %s, ваш заказ:%s готов к выдаче\n", user.Name, orderID)
 }
 
-type Sms struct{}
+// SMS обеспечивает взамодействие c sms сервисом
+type SMS struct{}
 
-// Отправляет уведомление пользователю по SMS
-func (s *Sms) SendsNotice(user *User, orderId string) {
+// SendsNotice отправляет уведомление пользователю по SMS
+func (s *SMS) SendsNotice(user *User, orderID string) {
 	// Здесь может содержаться вся бизнес-логика для отправки уведомления по SMS,
-	// у структуры Sms могут присутствовать подобные методы (GetPhoneByUserId, SmsServiceConnection)
-	fmt.Printf("[sms] %s, ваш заказ:%s готов к выдаче\n", user.Name, orderId)
+	// у структуры Sms могут присутствовать подобные методы (GetPhoneByUserID, SmsServiceConnection)
+	fmt.Printf("[sms] %s, ваш заказ:%s готов к выдаче\n", user.Name, orderID)
 }
