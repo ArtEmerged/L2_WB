a41f11d2b96b5e7898de743f9a3048ac2473b8af
Artyom_HP <titan4590it@gmail.com>
2024-02-16 20:50:28 +0300

    add: test and comments for func

--- develop/dev03/task.go
+++ develop/dev03/task.go
@@ -38,47 +38,47 @@ import (
 */
 
 var (
-	ErrArgs       = errors.New("sort: the number of argems has been exceeded")
+	ErrArgs       = errors.New("sort: the number of arguments has been exceeded")
 	ErrColumn     = errors.New("sort: invalid number in column option")
 	ErrNoSuchFile = errors.New("sort: no such file or directory")
 )
 
 type Parameters struct {
-	columnFlag int
-	numberFlag bool
-	reversFlag bool
-	uniqueFlag bool
-	args       []string
+	columnFlag  int
+	numberFlag  bool
+	reverseFlag bool
+	uniqueFlag  bool
+	args        []string
 }
 
 func main() {
-	// str, _ := strconv.Atoi("200hello")
-	// fmt.Println(str)
 	parameters := Parameters{}
-	flag.IntVar(&parameters.columnFlag, "k", 1, "")      // -k — указание колонки для сортировки
-	flag.BoolVar(&parameters.numberFlag, "n", false, "") // -n — сортировать по числовому значению
-	flag.BoolVar(&parameters.reversFlag, "r", false, "") // -r — сортировать в обратном порядке
-	flag.BoolVar(&parameters.uniqueFlag, "u", false, "") // -u — не выводить повторяющиеся строки
+	flag.IntVar(&parameters.columnFlag, "k", 1, "")       // -k — указание колонки для сортировки
+	flag.BoolVar(&parameters.numberFlag, "n", false, "")  // -n — сортировать по числовому значению
+	flag.BoolVar(&parameters.reverseFlag, "r", false, "") // -r — сортировать в обратном порядке
+	flag.BoolVar(&parameters.uniqueFlag, "u", false, "")  // -u — не выводить повторяющиеся строки
 	flag.Parse()
 
 	parameters.args = flag.Args()
-	err := Run(parameters)
+	res, err := Run(parameters)
 	if err != nil {
 		fmt.Fprint(os.Stderr, err)
 		os.Exit(1)
 	}
+
+	outputResult(res)
 }
 
-func Run(parameters Parameters) error {
+func Run(parameters Parameters) ([]string, error) {
 	// Проверяем правильность переданных аргументов
 	err := validate(parameters.columnFlag, parameters.args)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
-	splitLines, err := readFile(flag.Arg(0))
+	splitLines, err := readFile(parameters.args[0])
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	// Если флаг -u == true, то формируем множество из строк
@@ -86,15 +86,14 @@ func Run(parameters Parameters) error {
 		splitLines = leaveUnique(splitLines)
 	}
 
-	sort(splitLines, parameters.columnFlag, parameters.numberFlag, parameters.reversFlag)
-	outputResult(splitLines)
-	return nil
+	sort(splitLines, parameters.columnFlag, parameters.numberFlag, parameters.reverseFlag)
+	return splitLines, nil
 }
 
-// validate - Проверяет переданные аргументы на соответсвие требованям для корректонй работы программы.
-// Если ошибки небыли выявленны, то функция возвращает nil
+// validate - Проверяет переданные аргументы на соответствие требованиям для корректной работы программы.
+// Если ошибки не были выявлены, то функция возвращает nil
 func validate(columnFlag int, args []string) error {
-	// Если было переданно больше чем один аргумент, то возвращаем ошибку ErrArgs
+	// Если было передано больше чем один аргумент, то возвращаем ошибку ErrArgs
 	if len(args) != 1 {
 		return ErrArgs
 	}
@@ -107,26 +106,24 @@ func validate(columnFlag int, args []string) error {
 
 // leaveUnique - возвращает множество из строк
 func leaveUnique(lines []string) []string {
-	// Обявляем map set для нахождения уникальных строк
+	// Объявляем map set для нахождения уникальных строк
 	set := map[string]struct{}{}
+	// Объявляем новый срез для хранения множеств
+	setSlice := make([]string, 0, len(lines))
 	// Проходимся циклом по всем линиям
 	for i := 0; i < len(lines); i++ {
-		// Если строка уже есть в нашем множестве, то мы меняем её с последним елементом
-		// и уменьшаем длину среза на один
-		if _, ok := set[lines[i]]; ok {
-			// Меняем местами повторяющуюся строчку с последней
-			lines[i], lines[len(lines)-1] = lines[len(lines)-1], lines[i]
-			// Уменшьнам длину среза на 1
-			lines = lines[:len(lines)-1]
+		// Если строки ещё нет в нашем множестве, то мы кладем её в срез и в map
+		if _, ok := set[lines[i]]; !ok {
+			setSlice = append(setSlice, lines[i])
+			set[lines[i]] = struct{}{}
 		}
-		set[lines[i]] = struct{}{}
 	}
-	return lines
+	return setSlice
 }
 
-// readFile -  выполняет чтение из переданого файла
+// readFile -  выполняет чтение из переданного файла
 // возвращает []string - содержимое файла разделенное на \n, err == nil
-// если произошла ошибка при открытие файла, то возращает err
+// если произошла ошибка при открытии файла, то возвращает err
 func readFile(nameFile string) ([]string, error) {
 	// Открываем файл который клиент передал через аргумент
 	file, err := os.Open(nameFile)
@@ -140,28 +137,30 @@ func readFile(nameFile string) ([]string, error) {
 	// Объявляем переменную res для записи файла построчно
 	res := make([]string, 0)
 
-	// Обявляем новый сканер buffS
+	// Объявляем новый сканер buffS
 	buffS := bufio.NewScanner(file)
 
-	// Проходимя циклом по всему файлу
+	// Проходим циклом по всему файлу
 	for buffS.Scan() {
-		// Добавляем в переменую res построчные данные типа string c помощью метода Text()
+		// Добавляем в переменную res построчные данные типа string с помощью метода Text()
 		res = append(res, buffS.Text())
 	}
 
 	return res, buffS.Err()
 }
 
+// outputResult - выводит в stdout результат
 func outputResult(output []string) {
 	for _, line := range output {
 		fmt.Println(line)
 	}
 }
 
-func sort(lines []string, columnFlag int, numberFlag, reversFlag bool) {
+// sort - выполняет сортировку в соответствии с флагами
+func sort(lines []string, columnFlag int, numberFlag, reverseFlag bool) {
 	slices.SortFunc(lines, func(a, b string) int {
-		//Если флаг -r == true, то сортируем в обратной последовательности
-		if reversFlag {
+		// Если флаг -r == true, то сортируем в обратной последовательности
+		if reverseFlag {
 			return compare(b, a, columnFlag, numberFlag)
 		}
 		return compare(a, b, columnFlag, numberFlag)
@@ -169,40 +168,62 @@ func sort(lines []string, columnFlag int, numberFlag, reversFlag bool) {
 
 }
 
+// compare - сравнивает строки согласно переданным флагам
 func compare(a, b string, column int, numberFlag bool) int {
+	// Если column больше одного, то выполняем сортировку по колонке
 	if column > 1 {
+		// Разделяем a и b по пробелам
 		splitA := strings.Fields(a)
 		splitB := strings.Fields(b)
+		// Присваиваем a и b новые значения согласно колонке
 		if column <= len(splitA) && column <= len(splitB) {
 			a = splitA[column-1]
 			b = splitB[column-1]
 		}
 	}
+	// Если флаг numberFlag == true, то проверяем числа в строках
 	if numberFlag {
-		return compareNumbers(a, b)
+		comp, ok := compareNumbers(a, b)
+		if ok {
+			return comp
+		}
 	}
 
 	return strings.Compare(a, b)
 }
 
-func compareNumbers(a, b string) int {
-	var numA int
-	var numB int
-	for i, symbol := range a {
+// compareNumbers - сравнивает числа в префиксах строк a и b,
+// a > b return +1, true
+// a < b return -1, true
+// a == b return 0, true
+// если обе строки не содержат числа в префиксе, то возвращает false
+func compareNumbers(a, b string) (int, bool) {
+	var numA, indexA int
+	var numB, indexB int
+	// Проверяем циклом где заканчивается число
+	for _, symbol := range a {
 		if !unicode.IsDigit(symbol) {
-			numA, _ = strconv.Atoi(a[:i])
+			break
 		}
+		indexA++
 	}
-	for i, symbol := range b {
+	for _, symbol := range b {
 		if !unicode.IsDigit(symbol) {
-			numB, _ = strconv.Atoi(b[:i])
+			break
 		}
+		indexB++
 	}
+	// Возвращаем false если оба цикла завершились на первой итерации
+	if indexA == 0 && indexB == 0 {
+		return 0, false
+	}
+	numA, _ = strconv.Atoi(a[:indexA])
+	numB, _ = strconv.Atoi(b[:indexB])
 	if numA < numB {
-		return -1
+		return -1, true
 	}
 	if numA > numB {
-		return +1
+		return +1, true
 	}
-	return 0
+	return 0, true
 }
--- develop/dev03/task_test.go
+++ develop/dev03/task_test.go
@@ -1,1 +1,143 @@
 package main
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestSort(t *testing.T) {
+	tests := []struct {
+		name       string
+		parameters Parameters
+		want       string
+		wantErr    error
+	}{
+		{
+			name:       "basic_test1",
+			parameters: Parameters{1, false, false, false, []string{"./testdata/test1.txt"}},
+			want:       "./testdata/test1_want.txt",
+			wantErr:    nil,
+		},
+		{
+			name:       "basic_test2",
+			parameters: Parameters{1, false, false, false, []string{"./testdata/symbols_test2.txt"}},
+			want:       "./testdata/symbols_test2_want.txt",
+			wantErr:    nil,
+		},
+		{
+			name:       "basic_test3",
+			parameters: Parameters{1, false, false, false, []string{"./testdata/numbers_test3.txt"}},
+			want:       "./testdata/numbers_test3_want.txt",
+			wantErr:    nil,
+		},
+		{
+			name:       "basic_test4",
+			parameters: Parameters{1, false, false, false, []string{"./testdata/latin_alfovit_test5.txt"}},
+			want:       "./testdata/latin_alfovit_test5_want.txt",
+			wantErr:    nil,
+		},
+		{
+			name:       "basic_test5",
+			parameters: Parameters{1, false, false, false, []string{"./testdata/cyrillic_alphabet_test4.txt"}},
+			want:       "./testdata/cyrillic_alphabet_test4_want.txt",
+			wantErr:    nil,
+		},
+		{
+			name:       "revers_test",
+			parameters: Parameters{1, false, true, false, []string{"./testdata/latin_alfovit_test5.txt"}},
+			want:       "./testdata/latin_alfovit_test5_want_r.txt",
+			wantErr:    nil,
+		},
+		{
+			name:       "column_test_k3",
+			parameters: Parameters{3, false, false, false, []string{"./testdata/number_column_test6.txt"}},
+			want:       "./testdata/number_column_test6_want_k3.txt",
+			wantErr:    nil,
+		},
+		{
+			name:       "column_test_k5",
+			parameters: Parameters{5, false, false, false, []string{"./testdata/number_column_test6.txt"}},
+			want:       "./testdata/number_column_test6_want_k5.txt",
+			wantErr:    nil,
+		},
+		{
+			name:       "columnErr_test",
+			parameters: Parameters{0, false, false, false, []string{"./testdata/number_column_test6.txt"}},
+			want:       "",
+			wantErr:    ErrColumn,
+		},
+		{
+			name:       "argsErr_test1",
+			parameters: Parameters{1, false, false, false, []string{"./testdata/number_column_test6.txt", "hello"}},
+			want:       "",
+			wantErr:    ErrArgs,
+		},
+		{
+			name:       "argsErr_test2",
+			parameters: Parameters{1, false, false, false, []string{}},
+			want:       "",
+			wantErr:    ErrArgs,
+		},
+		{
+			name:       "noSuchFileErr_test2",
+			parameters: Parameters{1, false, false, false, []string{"sffwuwuwwrwo0322"}},
+			want:       "",
+			wantErr:    ErrNoSuchFile,
+		},
+		{
+			name:       "nubmers_test",
+			parameters: Parameters{1, true, false, false, []string{"./testdata/numbers_test3.txt"}},
+			want:       "./testdata/numbers_test3_want_n.txt",
+			wantErr:    nil,
+		},
+		{
+			name:       "nubmers+column_test",
+			parameters: Parameters{3, true, false, false, []string{"./testdata/numbers_test3.txt"}},
+			want:       "./testdata/numbers_test3_want_nk3.txt",
+			wantErr:    nil,
+		},
+		{
+			name:       "nubmers+column+revers_test",
+			parameters: Parameters{3, true, true, false, []string{"./testdata/numbers_test3.txt"}},
+			want:       "./testdata/numbers_test3_want_nk3r.txt",
+			wantErr:    nil,
+		},
+		{
+			name:       "unique_test",
+			parameters: Parameters{1, false, false, true, []string{"./testdata/test1.txt"}},
+			want:       "./testdata/test1_want_u.txt",
+			wantErr:    nil,
+		},
+		{
+			name:       "all_flags_test",
+			parameters: Parameters{3, true, true, true, []string{"./testdata/all_flags_test.txt"}},
+			want:       "./testdata/all_flags_test_want.txt",
+			wantErr:    nil,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			got, gotErr := Run(test.parameters)
+
+			if gotErr != nil {
+				if got != nil {
+					t.Errorf("Run() got = %v , want = %v", got, nil)
+				}
+				if gotErr != test.wantErr {
+					t.Errorf("Run() goterr = %v , want = %v", gotErr, test.wantErr)
+				}
+				return
+			}
+
+			if gotErr != test.wantErr {
+				t.Errorf("Run() goterr = %v , want = %v", gotErr, test.wantErr)
+			}
+			wantText, _ := readFile(test.want)
+			if strings.Join(got, "\n") != strings.Join(wantText, "\n") {
+				t.Errorf("Run() got = \n%v\n , want = \n%v", got, wantText)
+			}
+
+		})
+	}
+}
