00b3129656638a69087d1d290d0f597693d46e9f
Artyom_HP <titan4590it@gmail.com>
2024-02-17 17:48:03 +0300

    add: dev 4

--- develop/dev04/task.go
+++ develop/dev04/task.go
@@ -1,5 +1,13 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+	"os"
+	"slices"
+	"strings"
+)
+
 /*
 === Поиск анаграмм по словарю ===
 
@@ -19,6 +27,95 @@ package main
 Программа должна проходить все тесты. Код должен проходить проверки go vet и golint.
 */
 
-func main() {
+var (
+	// ErrLetter - ошибка некорректного ввода буквы
+	ErrLetter = errors.New("anagram: incorrect letter entry")
+)
 
+// sortSlice - сортирует срезы строк в map
+func sortSlice(anagramSets map[string][]string) {
+	for _, words := range anagramSets {
+		slices.Sort(words)
+	}
+}
+
+// generatesAnagram - генерирует уникальный ключ для анаграмм
+func generatesAnagram(word string) ([33]uint8, error) {
+	// Создаем массив из 33 элементов, у каждой буквы свой индекс
+	anagram := [33]uint8{}
+	for _, letter := range word {
+		// Русский алфавит начинается с 1072 руны - буква 'а'
+		// Определяем позицию буквы в массиве
+		index := int(letter - 'а')
+		// Проверка на русские буквы нижнего регистра
+		if index < 0 || index > 32 {
+			return anagram, ErrLetter
+		}
+		anagram[index]++
+	}
+	return anagram, nil
+}
+
+func main() {
+	arr := []string{"пятак", "тяпка", "пятка", "пудра", "пятка", "листок", "мох", "ГоРа","рОга", "слиток", "столик"}
+	res, err := searchForAnagramSets(&arr)
+	if err != nil {
+		fmt.Fprint(os.Stderr, err)
+		os.Exit(1)
+	}
+	fmt.Println(res)
+}
+
+// searchForAnagramSets - выполняет поиск всех множеств анаграмм по словарю.
+// Функция возвращает отсортированное множество анаграмм  map[string][]string.
+// Если в функцию передан словарь не с русскими буквами, то функция возвращает ошибку ErrLetter.
+func searchForAnagramSets(words *[]string) (*map[string][]string, error) {
+	// Объявляем map result для хранения отсортированных множеств анаграмм
+	result := make(map[string][]string)
+	// Объявляем map для хранения анаграмм
+	anagrams := make(map[[33]uint8][]string)
+	// Объявляем set для уникальных слов
+	set := make(map[string]struct{})
+
+	for _, word := range *words {
+		// Переводим слово в нижний регистр
+		word = strings.ToLower(word)
+
+		if _, ok := set[word]; ok {
+			continue
+		}
+		set[word] = struct{}{}
+
+		// Генерируем уникальный массив для хранения анаграмм
+		anagramWord, err := generatesAnagram(word)
+		// Возвращаем ошибку, если буква не соответствует требованиям
+		if err != nil {
+			return nil, err
+		}
+
+		// Добавляем слово в массив анаграмм
+		anagrams[anagramWord] = append(anagrams[anagramWord], word)
+
+		// Если в массиве анаграмм больше одного слова, то добавляем его во множество анаграмм
+		if len(anagrams[anagramWord]) > 1 {
+
+			// Присваиваем переменной firstWord первое слово из массива анаграмм
+			firstWord := anagrams[anagramWord][0]
+
+			// Если во множестве анаграмм еще нет конкретной анаграммы, то добавляем первое слово
+			if _, ok := result[firstWord]; !ok {
+				// Добавляем первое слово во множество анаграмм
+				result[firstWord] = append(result[firstWord], firstWord)
+			}
+
+			// Присваиваем переменной lastWord последнее слово из массива анаграмм
+			lastWord := anagrams[anagramWord][len(anagrams[anagramWord])-1]
+
+			// Добавляем последнее слово во множество анаграмм
+			result[firstWord] = append(result[firstWord], lastWord)
+		}
+	}
+	sortSlice(result)
+
+	return &result, nil
 }
--- develop/dev04/task_test.go
+++ develop/dev04/task_test.go
@@ -1,1 +1,49 @@
 package main
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestAnagram(t *testing.T) {
+	testsCases := []struct {
+		name       string
+		dictionary []string
+		want       map[string][]string
+		wantErr    error
+	}{
+		{
+			name:       "basic_test",
+			dictionary: []string{"пятак", "тяпка", "пятка", "пудра", "пятка", "листок", "мох", "ГоРа", "рОга", "слиток", "столик"},
+			want:       map[string][]string{"гора": {"гора", "рога"}, "листок": {"листок", "слиток", "столик"}, "пятак": {"пятак", "пятка", "тяпка"}},
+			wantErr:    nil,
+		},
+		{
+			name:       "error_test",
+			dictionary: []string{"пятак", "тяпка", "пяtка", "hello", "пудра", "пятка", "листок", "мох", "ГоРа", "рОга", "слиток", "столик"},
+			want:       nil,
+			wantErr:    ErrLetter,
+		},
+		{
+			name:       "empty_test",
+			dictionary: []string{},
+			want:       map[string][]string{},
+			wantErr:    nil,
+		},
+	}
+	for _, test := range testsCases {
+		t.Run(test.name, func(t *testing.T) {
+			got, gotErr := searchForAnagramSets(&test.dictionary)
+			if gotErr != test.wantErr {
+				t.Errorf("gotErr %v, want %v", gotErr, test.wantErr)
+			}
+			if gotErr != nil {
+				return
+			}
+			if !reflect.DeepEqual(*got, test.want) {
+				t.Errorf("got %v, want %v", *got, test.want)
+			}
+		})
+
+	}
+}
